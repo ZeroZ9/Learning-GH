3466dec2fb2fa256b1bde04e29a1b750b0a75d44
Trần Hoàng Long <tranlonguuu@gmail.com>
2022-10-09 18:49:44 -0500

    Update ConentView to Waht's up

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello mi dua!")
+            Text("What's up")
         }
         .padding()
     }
